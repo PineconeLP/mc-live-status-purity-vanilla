03f8e84a472dabc0fc1c21111db9e5680f89c749
PineconeLP <pineconelp4@gmail.com>
2020-10-20 09:46:42 -0400

    Try retrieving player names

--- pinger/Scripts/main.py
+++ pinger/Scripts/main.py
@@ -1,9 +1,8 @@
 import sys
-import json
 from get_server import get_server
 
 server = get_server(sys.argv)
 
 info = server.status()
 
-print(json.dumps({"online": info.players.online, "max": info.players.max}))
+print(info.raw['players'])
