36bbe79db0f8bae679571d3feee8a36dc0507212
PineconeLP <pineconelp4@gmail.com>
2020-12-13 21:21:13 -0500

    Add login UI

--- blazor-purity-vanilla/Components/Authentication/Login.razor.cs
+++ blazor-purity-vanilla/Components/Authentication/Login.razor.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Threading.Tasks;
+using Endpointer.Authentication.Core.Models.Requests;
 using MCLiveStatus.PurityVanilla.Blazor.Stores.Authentication;
 using Microsoft.AspNetCore.Components;
 
@@ -7,5 +10,19 @@ namespace MCLiveStatus.PurityVanilla.Blazor.Components.Authentication
     {
         [Inject]
         public AuthenticationStore AuthenticationStore { get; set; }
+
+        private LoginRequest LoginRequest { get; } = new LoginRequest();
+
+        private async Task OnLogin()
+        {
+            try
+            {
+                await AuthenticationStore.Login(LoginRequest.Username, LoginRequest.Password);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
     }
 }
