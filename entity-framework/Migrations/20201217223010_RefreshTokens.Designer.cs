e461ce1722a45397cece56b2e51fd02fe8814833
PineconeLP <pineconelp4@gmail.com>
2020-12-17 17:32:42 -0500

    Add refresh token migrations

--- entity-framework/Migrations/20201217223010_RefreshTokens.Designer.cs
+++ entity-framework/Migrations/20201217223010_RefreshTokens.Designer.cs
@@ -2,14 +2,16 @@
 using MCLiveStatus.EntityFramework.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MCLiveStatus.EntityFramework.Migrations
 {
     [DbContext(typeof(MCLiveStatusDbContext))]
-    partial class MCLiveStatusDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201217223010_RefreshTokens")]
+    partial class RefreshTokens
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
