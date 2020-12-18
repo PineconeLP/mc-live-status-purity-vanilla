0799e54eeedfcbcc83dcb7a5b0264e81ea95a68c
PineconeLP <pineconelp4@gmail.com>
2020-12-17 20:05:26 -0500

    Add register validation

--- blazor-purity-vanilla/Components/Authentication/Register.razor.cs
+++ blazor-purity-vanilla/Components/Authentication/Register.razor.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Endpointer.Authentication.Client.Services;
 using Endpointer.Authentication.Core.Models.Requests;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace MCLiveStatus.PurityVanilla.Blazor.Components.Authentication
 {
@@ -14,27 +15,45 @@ namespace MCLiveStatus.PurityVanilla.Blazor.Components.Authentication
         [Inject]
         public NavigationManager NavigationManager { get; set; }
 
+        private EditContext _registerEditContext;
+
         private RegisterRequest RegisterRequest { get; } = new RegisterRequest();
+
+        private bool CanNotRegister => string.IsNullOrEmpty(RegisterRequest.Email) ||
+            string.IsNullOrEmpty(RegisterRequest.Username) ||
+            string.IsNullOrEmpty(RegisterRequest.Password) ||
+            string.IsNullOrEmpty(RegisterRequest.ConfirmPassword);
 
         private bool IsRegistering { get; set; }
 
+        protected override void OnInitialized()
+        {
+            _registerEditContext = new EditContext(RegisterRequest);
+
+            base.OnInitialized();
+        }
+
         private async Task OnRegister()
         {
-            IsRegistering = true;
-            StateHasChanged();
+            if (_registerEditContext.Validate())
+            {
 
-            try
-            {
-                await RegisterService.Register(RegisterRequest);
-                NavigationManager.NavigateTo("/login");
-            }
-            catch (Exception)
-            {
-            }
-            finally
-            {
-                IsRegistering = false;
+                IsRegistering = true;
                 StateHasChanged();
+
+                try
+                {
+                    await RegisterService.Register(RegisterRequest);
+                    NavigationManager.NavigateTo("/login");
+                }
+                catch (Exception)
+                {
+                }
+                finally
+                {
+                    IsRegistering = false;
+                    StateHasChanged();
+                }
             }
         }
     }
